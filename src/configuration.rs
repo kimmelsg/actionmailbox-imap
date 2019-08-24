0d323e9b9229bafd623b01fa960372dcb3138786
Ethan Knowlton <eknowlton@gmail.com>
2019-08-24 15:59:38 -0400

    Feature/rust imap ingress command (#7)
    * Apparently rails didnt test their tasks...
    
    * config file is a little different
    
    * touch ups to actually work with rust client
    
    * basically only a rails task and a yaml file now
    
    * added rust project
    
    * read me and client readme updates
    
    * no tests?
    
    * this binary was not needed
    
    * updated gem version number

---
+++ src/configuration.rs
@@ -0,0 +1,42 @@
+use config::{Config, ConfigError, File};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct ConfigObject {
+    server: String,
+    port: i64,
+    tls: bool,
+    username: String,
+    password: String,
+    mailbox: String,
+}
+
+impl ConfigObject {
+    pub fn new(filename: &str) -> Result<ConfigObject, ConfigError> {
+        let mut config = Config::new();
+
+        config.merge(File::with_name(filename))?;
+
+        config.try_into::<ConfigObject>()
+    }
+
+    pub fn server(&self) -> &String {
+        &self.server
+    }
+
+    pub fn port(&self) -> &i64 {
+        &self.port
+    }
+
+    pub fn username(&self) -> &String {
+        &self.username
+    }
+
+    pub fn password(&self) -> &String {
+        &self.password
+    }
+
+    pub fn mailbox(&self) -> &String {
+        &self.mailbox
+    }
+}
